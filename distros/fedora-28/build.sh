00a3a2fcc847edca942c02a78dc995a153036375
root <root@linux-81ry.amd.com>
2018-08-01 16:31:09 -0500

    test

--- distros/fedora-28/build.sh
+++ distros/fedora-28/build.sh
@@ -7,9 +7,9 @@ run_cmd "yum install yum-utils"
 run_cmd "yum-builddep qemu"
 
 # build and install QEMU 2.12
-build_qemu "/usr/local"
+build_install_qemu "/usr/local"
 
 run_cmd "cp ../launch-qemu.sh /usr/local/bin"
 
 # fix path to pick rebuild BIOS
-sed -i 's|UEFI_BIOS_CODE="/usr/local/share/qemu/OVMF_CODE.fd"|UEFI_BIOS_CODE=/usr/share/qemu//usr/share/OVMF/OVMF_CODE.secboot.fd|' /usr/local/bin/launch-qemu.sh
+sed -i 's|UEFI_BIOS_CODE="/usr/local/share/qemu/OVMF_CODE.fd"|UEFI_BIOS_CODE=/usr/share/OVMF/OVMF_CODE.secboot.fd|' /usr/local/bin/launch-qemu.sh
