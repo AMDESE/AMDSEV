9239045e317fc1ea17fefb294f50ed49b02e1ec6
0.alpha <meJustPlay@163.com>
2022-05-11 20:35:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 [ -e /etc/os-release ] && . /etc/os-release
 
 # This will install all the dependent packages for qemu and ovmf to run
-if [ "$ID_LIKE" = "debian" ]; then
+if [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ]; then
 	apt-get -y install qemu ovmf
 else
 	dnf install qemu edk2-ovmf
 fi
 
-if [ "$ID_LIKE" = "debian" ]; then
+if [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ]; then
 	dpkg -i linux/host/linux-image-*.deb
 else
 	rpm -ivh linux/kernel-*.rpm
