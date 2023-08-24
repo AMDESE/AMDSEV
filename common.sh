AMDESE/AMDSEV

commit 4076475b82734a80906eba353245aacb8a040025

Improve handling git remote/branch updates for existing repos
OVMF/QEMU don't properly update to newer branches/repos if they've been
previously cloned. Existing kernel repo does handle updating to newer
branches, but not when the git remote/repo changes.

Nuking the OVMF/QEMU/kernel repos with each update of build scripts is
still the safest/cleanest way, but it is costly on time/bandwidth, so
improve the scripts handle these scenarios when there are stale git
repos present.

Signed-off-by: Michael Roth <michael.roth@amd.com>

common.sh
@@ -44,6 +44,18 @@ build_kernel()
 			BRANCH="${KERNEL_HOST_BRANCH}"
 		fi
 
+		# If ${KERNEL_GIT_URL} is ever changed, 'current' remote will be out
+		# of date, so always update the remote URL first. Also handle case
+		# where AMDSEV scripts are updated while old kernel repos are still in
+		# the directory without a 'current' remote
+		pushd ${V} >/dev/null
+		if git remote get-url current 2>/dev/null; then
+			run_cmd git remote set-url current ${KERNEL_GIT_URL}
+		else
+			run_cmd git remote add current ${KERNEL_GIT_URL}
+		fi
+		popd >/dev/null
+
 		# Nuke any previously built packages so they don't end up in new tarballs
 		# when ./build.sh --package is specified
 		rm -f linux-*-snp-${V}*
@@ -55,9 +67,6 @@ build_kernel()
 		run_cmd $MAKE distclean
 
 		pushd ${V} >/dev/null
-			# If ${KERNEL_GIT_URL} is ever changed, 'current' remote will be out
-			# of date, so always update the remote URL first
-			run_cmd git remote set-url current ${KERNEL_GIT_URL}
 			run_cmd git fetch current
 			run_cmd git checkout current/${BRANCH}
 			COMMIT=$(git log --format="%h" -1 HEAD)
@@ -116,15 +125,24 @@ build_install_ovmf()
 
 	BUILD_CMD="nice build -q --cmd-len=64436 -DDEBUG_ON_SERIAL_PORT=TRUE -n $(getconf _NPROCESSORS_ONLN) ${GCCVERS:+-t $GCCVERS} -a X64 -p OvmfPkg/OvmfPkgX64.dsc"
 
-	[ -d ovmf ] || {
-		run_cmd git clone --single-branch -b ${OVMF_BRANCH} ${OVMF_GIT_URL} ovmf
-
+	# initialize git repo, or update existing remote to currently configured one
+	if [ -d ovmf ]; then
 		pushd ovmf >/dev/null
-			run_cmd git submodule update --init --recursive
+		if git remote get-url current 2>/dev/null; then
+			run_cmd git remote set-url current ${OVMF_GIT_URL}
+		else
+			run_cmd git remote add current ${OVMF_GIT_URL}
+		fi
 		popd >/dev/null
-	}
+	else
+		run_cmd git clone --single-branch -b ${OVMF_BRANCH} ${OVMF_GIT_URL} qemu
+		run_cmd git remote add current ${OVMF_GIT_URL}
+	fi
 
 	pushd ovmf >/dev/null
+		run_cmd git fetch current
+		run_cmd git checkout current/${OVMF_BRANCH}
+		run_cmd git submodule update --init --recursive
 		run_cmd make -C BaseTools
 		. ./edksetup.sh --reconfig
 		run_cmd $BUILD_CMD
@@ -139,11 +157,25 @@ build_install_qemu()
 {
 	DEST="$1"
 
-	[ -d qemu ] || run_cmd git clone --single-branch -b ${QEMU_BRANCH} ${QEMU_GIT_URL} qemu
+	# initialize git repo, or update existing remote to currently configured one
+	if [ -d qemu ]; then
+		pushd qemu >/dev/null
+		if git remote get-url current 2>/dev/null; then
+			run_cmd git remote set-url current ${QEMU_GIT_URL}
+		else
+			run_cmd git remote add current ${QEMU_GIT_URL}
+		fi
+		popd >/dev/null
+	else
+		run_cmd git clone --single-branch -b ${QEMU_BRANCH} ${QEMU_GIT_URL} qemu
+		run_cmd git remote add current ${QEMU_GIT_URL}
+	fi
 
 	MAKE="make -j $(getconf _NPROCESSORS_ONLN) LOCALVERSION="
 
 	pushd qemu >/dev/null
+		run_cmd git fetch current
+		run_cmd git checkout current/${QEMU_BRANCH}
 		run_cmd ./configure --target-list=x86_64-softmmu --prefix=$DEST
 		run_cmd $MAKE
 		run_cmd $MAKE install

commit 12e6ccc13eb098b498171b18fd651d3468f072b9

Fix initial cloning of ovmf repo
Fresh checkouts of ovmf recently broke with commit 4076475b, so only
builds with previously-cloned ovmf repos are currently working. Fix
this.

Signed-off-by: Michael Roth <michael.roth@amd.com>

common.sh
@@ -136,7 +136,7 @@ build_install_ovmf()
 		fi
 		popd >/dev/null
 	else
-		run_cmd git clone --single-branch -b ${OVMF_BRANCH} ${OVMF_GIT_URL} qemu
+		run_cmd git clone --single-branch -b ${OVMF_BRANCH} ${OVMF_GIT_URL} ovmf
 		run_cmd git remote add current ${OVMF_GIT_URL}
 	fi
 